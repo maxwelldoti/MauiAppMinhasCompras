d72ffe7f179f690988fe71d86512495334c3762c
maxwelldoti <maxwell.lima@fatec.sp.gov.br>
2025-03-19 11:41:44 -0300

    finalização do basico do aplicatico remover e semecionar item da lista

--- MauiAppMinhasCompras/App.xaml.cs
+++ MauiAppMinhasCompras/App.xaml.cs
@@ -4,36 +4,32 @@ namespace MauiAppMinhasCompras
 {
     public partial class App : Application
     {
-
         static SQLiteDatabaseHelper _db;
-        
-        public static SQLiteDatabaseHelper DB
+
+        public static SQLiteDatabaseHelper Db
         {
             get
             {
                 if (_db == null)
                 {
                     string path = Path.Combine(
-                    Environment.GetFolderPath(
-                        Environment.SpecialFolder.LocalApplicationData),
+                        Environment.GetFolderPath(
+                            Environment.SpecialFolder.LocalApplicationData),
                         "banco_sqlite_compras.db3");
 
-                    _db = new SQLiteDatabaseHelper(path);                   
+                    _db = new SQLiteDatabaseHelper(path);
                 }
 
                 return _db;
             }
-
-
         }
+
         public App()
         {
             InitializeComponent();
 
             //MainPage = new AppShell();
-
             MainPage = new NavigationPage(new Views.ListaProduto());
-
         }
     }
 }
